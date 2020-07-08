d55d93dddef2eef95ca49afdb6974cf364242f8f
Caroline Ninganga <carolninganga@gmail.com>
2020-07-08 06:49:31 -0400

    added button copmonents css

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,8 +1,9 @@
 import React from 'react'
 
-export default function Button() {
+export default function Button(props) {
     return (
-        <div>
+        <div className="text-center mt-3">
+            <button type="button" onclick={props.handleClick} className="btn btn-primary">Click for Another Random Joke</button>
             
         </div>
     )
