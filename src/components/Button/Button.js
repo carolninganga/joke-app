2fd9f4eb2fffa4846b865ba04f58944a977c7a49
Caroline Ninganga <carolninganga@gmail.com>
2020-07-08 07:32:09 -0400

    added an onclick to button component

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -4,7 +4,7 @@ import './Button.css'
 function Button(props) {
     return (
         <div className="text-center mt-3">
-            <button type="button" onclick={props.handleClick} className="btn btn-primary">Click for Another Random Joke</button>
+            <button type="button" onClick={props.handleClick} className="btn btn-primary">Click for Another Random Joke</button>
             
         </div>
     )
