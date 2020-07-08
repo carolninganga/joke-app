5f2fe1b069fc62b6f2a06cae1daa127f68880f5a
Caroline Ninganga <carolninganga@gmail.com>
2020-07-08 06:44:46 -0400

    added h1 title to home page

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
   return (
     <React.Fragment>
-    <h1 className="text-center mt-3"><span role="img" aria-label="Joy">😂</span>Joke Joke<span role="img" aria-label="Joy">😂</span></h1> 
+    <h1 className="text-center mt-3"><span role="img" aria-label="Joy">😂</span>Joke Jokes and more Jokes!!<span role="img" aria-label="Joy">😂</span></h1> 
     <Card />
     </React.Fragment>
   );
