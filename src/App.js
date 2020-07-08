93c3e8cebd73149ed2dc4d1c31b8feebc8ce9709
Caroline Ninganga <carolninganga@gmail.com>
2020-07-08 06:59:17 -0400

    added body gradient background

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import Card from './components/Card/Card';
 import './App.css';
+import Button from './components/Button/Button';
 
 class App extends Component {
   render() {
   return (
     <React.Fragment>
-    <h1 className="text-center mt-3"><span role="img" aria-label="Joy">😂</span>Joke Jokes and more Jokes!!<span role="img" aria-label="Joy">😂</span></h1> 
+    <h1 className="text-center mt-3"><span role="img" aria-label="Joy">😂😂😂</span>Joke Jokes and more Jokes!!<span role="img" aria-label="Joy">😂😂😂</span></h1> 
     <Card />
+    <Button />
     </React.Fragment>
   );
 }
--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React from 'react';
+import './Button.css'
 
-export default function Button(props) {
+function Button(props) {
     return (
         <div className="text-center mt-3">
             <button type="button" onclick={props.handleClick} className="btn btn-primary">Click for Another Random Joke</button>
@@ -8,3 +9,5 @@ export default function Button(props) {
         </div>
     )
 }
+
+export default Button;
